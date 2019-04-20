c55e792ab3a6af58202285979f7dc432faf48caf
soopercool101 <soopercoolstages@gmail.com>
2019-04-20 15:49:13 -0700

    Add JellyFish Movement script

---
+++ FunChallenge/Assets/Jellyfish.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jellyfish : MonoBehaviour
+{
+    [Header("Settings")]
+    // the key used to activate the push
+    public KeyCode key = KeyCode.Space;
+
+    [Header("Movement")]
+    [Tooltip("Speed of movement")]
+    public float speed = 5f;
+
+    private Vector2 movement, cachedDirection;
+    private float moveHorizontal;
+    private float moveVertical;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    // FixedUpdate is called every frame when the physics are calculated
+    void FixedUpdate()
+    {
+        if (Input.GetKey(key))
+        {
+            GetComponent<Rigidbody2D>().freezeRotation = true;
+            GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(0, speed));
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().freezeRotation = false;
+            GetComponent<Rigidbody2D>().MoveRotation(GetComponent<Rigidbody2D>().rotation + 1);
+        }
+    }
+}
