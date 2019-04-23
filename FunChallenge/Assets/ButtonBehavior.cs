219858b71812b3491917b015caa9c7376e040019
rtustison <rachaeltustison@gmail.com>
2019-04-22 21:44:24 -0700

    Menu and Win Screen added

---
+++ FunChallenge/Assets/ButtonBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehavior : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Pressed!");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
